d12dc237424a1cf3810ed0851a2057e1172149d8
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-10 16:20:52 -0300

    requisito 21 ao 23

---
+++ challenges/desafio22.js
@@ -0,0 +1,12 @@
+db.voos.findOne({ 
+  $and: 
+  [
+    { "aeroportoOrigem.sigla": { $eq: "SBGR" } },
+    { "aeroportoDestino.sigla": { $eq: "KJFK" } }, 
+      { $or: 
+      [
+        { "empresa.nome": { $eq: "DELTA AIRLINES" } },
+        { "empresa.nome": { $eq: "AMERICAN AIRLINES" } }
+      ] } 
+  ] },
+{ vooId: 1, _id: 0 });
