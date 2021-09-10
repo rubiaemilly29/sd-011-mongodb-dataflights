05f866fe48151531086a3e8cf50c531d32c2b5c2
Thiagofox <thiagofox00@gmail.com>
2021-09-10 17:15:29 -0300

    desafio 13 e 14 finalizados

---
+++ challenges/desafio13.js
@@ -0,0 +1,3 @@
+// 13 - Retorne a quantidade de documentos em que o campo aeroportoDestino.continente não seja igual a EUROPA, ÁSIA e OCEANIA.
+
+db.voos.find({ "aeroportoDestino.continente": { $in: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
