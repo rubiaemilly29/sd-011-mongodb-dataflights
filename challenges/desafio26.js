6d5dfddbeb49e10f10cc3e0ff5154196bbf39023
Hugosomers <hugosomersla@gmail.com>
2021-09-10 18:26:29 -0300

    Fix desafio 26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,2 +1,2 @@
-db.voos.deleteMany({ "empresa.nome": "GOL", "passageiros.pagos": { $gte: 400, $lte: 10 } }, 
+db.voos.deleteMany({ "empresa.nome": "GOL", "passageiros.pagos": { $gte: 5, $lte: 10 } }, 
 { acknowledged: 0 });
