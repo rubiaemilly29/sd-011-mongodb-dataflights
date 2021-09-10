e987d3a00a6d59a1041d9b4accfe3093f375e492
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-09-10 17:34:24 -0300

    corrigindo linhas com mais de cem caracteres

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,2 +1,3 @@
 db.voos.count({$and: [{"aeroportoDestino.continente": {$ne: "EUROPA"}},
-{"aeroportoDestino.continente": {$ne: "ÁSIA"}}, {"aeroportoDestino.continente": {$ne: "OCEANIA"}}]});
+{"aeroportoDestino.continente": {$ne: "ÁSIA"}},
+{"aeroportoDestino.continente": {$ne: "OCEANIA"}}]});
