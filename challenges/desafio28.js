b9e4e2fa9dc9d4f22ec409c911e38e21ba8e4752
menezesrenan7 <renan_menezes07@hotmail.com>
2021-09-10 18:14:58 -0300

    Ajustes ESLint.

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,6 +1,6 @@
 db.resumoVoos.insertMany([{
   empresa: "LATAM AIRLINES BRASIL",
-  totalVoosDomesticos: 20026
+  totalVoosDomesticos: 20026,
 }]);
 db.resumoVoos.find({
-  empresa: "LATAM AIRLINES BRASIL"}, {_id: 0, empresa: 1, totalVoosDomesticos: 1}).limit(1);
+  empresa: "LATAM AIRLINES BRASIL" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 }).limit(1);
