55241e3def6fce39b0cc3eef1934ac1ac21c9482
anaclarabck <ackyotoku@gmail.com>
2021-09-10 16:58:35 -0300

    [ADD] challenges 24~28

---
+++ challenges/desafio28.js
@@ -0,0 +1,7 @@
+const LATAM = "LATAM AIRLINES BRASIL";
+db.resumoVoos.insertOne({
+empresa: LATAM,
+totalVoosDomesticos: db.voos.find({
+"empresa.nome": LATAM,
+natureza: "Doméstica" }).count() });
+db.resumoVoos.find({ empresa: LATAM });
