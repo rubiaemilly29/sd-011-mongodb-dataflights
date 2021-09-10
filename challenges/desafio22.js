b1fdbb9b8729745323cba41187bf783f18678974
Arthur Hermann <arthurhermannc@gmail.com>
2021-09-10 15:33:55 -0300

    r22

---
+++ challenges/desafio22.js
@@ -0,0 +1,7 @@
+db.voos.findOne(
+{ "empresa.nome": 
+{ $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+"aeroportoOrigem.sigla": "SBGR",
+"aeroportoDestino.sigla": "KJFK" },
+{ vooId: true, _id: false },
+);
