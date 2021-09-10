168c18495c9ae12d4718c3a75f3cc4a37d2e48e9
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-09-10 17:39:34 -0300

    [ADD] req 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,11 @@
+db.voos.findOne({ 
+  $and: [ 
+    { litrosCombustivel: { $not: { $gt: 600 } } }, 
+   { $or:
+    [
+      { "empresa.nome": { $not: { $eq: "GOL" } } }, 
+      { "empresa.nome": { $not: { $eq: "AZUL" } } },
+    ] },
+  { litrosCombustivel: { $exists: true } },
+  ] }, 
+  { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
