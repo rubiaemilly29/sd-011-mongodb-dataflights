615a801ac32ac28ccaeadc5d6fb0ce508592f413
marcoswarmling <marcoswarmling15@gmail.com>
2021-09-10 18:29:10 -0300

    corrigindo o 22 e agora sim projeto finalizado!!!

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,9 +1,10 @@
 db.voos.find({
-    "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
-},
-    {
-        $and:
-            [{ "aeroportoOrigem.sigla": "SBGR" },
-            { "aeroportoDestino.sigla": "KJFK" },
-            ],
-    }, { vooId: 1, _id: 0 }).limit(1); 
+    $and: [{
+        "empresa.nome": {
+            $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"]
+        }
+    },
+    { "aeroportoOrigem.sigla": "SBGR" },
+    { "aeroportoDestino.sigla": "KJFK" },
+    ],
+}, { vooId: 1, _id: 0 }).limit(1);
