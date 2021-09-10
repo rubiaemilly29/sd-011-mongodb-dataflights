8e1e1cd986b1178f2e2d87a5dc40f1d17fb7ae6d
anaclarabck <ackyotoku@gmail.com>
2021-09-10 17:17:49 -0300

    [UPDATE] fixed challenge 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,2 +1,2 @@
-db.voos.find({ litrosCombustivel: { $lt: 1000 } },
+db.voos.find({ litrosCombustivel: { $lt: 600 }, "empresa.nome": { $nin: ["GOL", "AZUL"] } },
 { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }).limit(1);
