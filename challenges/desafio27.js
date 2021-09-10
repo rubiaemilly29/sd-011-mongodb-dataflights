2511543b33bb001f7bf4bc58776699b0743b671c
Víctor Faria <vitilevi@hotmail.com>
2021-09-10 16:59:52 -0300

    requisito 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,13 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.count({
+    $and: [
+      {
+        "empresa.nome": "PASSAREDO",
+      },
+      {
+        natureza: "Doméstica",
+      },
+    ],
+  }),
+});
