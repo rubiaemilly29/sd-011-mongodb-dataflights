7b9c8a7f3dd67f1ccb4c0d20015fd0924a477a5e
Thyago Pessoa <flash_thyago@hotmail.com>
2021-09-10 16:58:32 -0300

    Exercicio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,11 @@
+const latam = "LATAM AIRLINES BRASIL";
+db.resumoVoos.insertMany([
+  {  
+  empresa: latam,
+  totalVoosDomesticos: db.voos.find({ $and: [
+    { "empresa.nome": { $eq: latam } }, 
+    { natureza: { $eq: "Doméstica" } }] }).count(),
+  },
+]);
+
+db.resumoVoos.find({ empresa: { $eq: latam } }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
