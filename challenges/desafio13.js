eb1f776d08f6f60f01cb1b7ec8e73977ec42d142
Thiagofox <thiagofox00@gmail.com>
2021-09-10 17:19:52 -0300

    desafio 13 e 14 finalizados 2

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,3 +1,3 @@
 // 13 - Retorne a quantidade de documentos em que o campo aeroportoDestino.continente não seja igual a EUROPA, ÁSIA e OCEANIA.
 
-db.voos.find({ "aeroportoDestino.continente": { $in: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
+db.voos.find({ "aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] } }).count();
--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,3 +1,3 @@
 // 14 - Retorne o total de voos em que o país de origem não seja BRASIL.
 
-db.voos.find({ "aeroportoDestino.pais": { $ne: "BRASIL" } }).count();
+db.voos.find({ "aeroportoOrigem.pais": { $ne: "BRASIL" } }).count();
