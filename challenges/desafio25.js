ee1edd5753ae30e65a86b740bbd2ef6fccb19929
Hugosomers <hugosomersla@gmail.com>
2021-09-10 18:02:21 -0300

    Fix LINT

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,1 +1,2 @@
-db.voos.deleteMany({ "empresa.nome": "AZUL", litrosCombustivel: { $lt: 400 } }, { acknowledged: 0 });
+db.voos.deleteMany({ "empresa.nome": "AZUL", litrosCombustivel: { $lt: 400 } }, 
+{ acknowledged: 0 });
