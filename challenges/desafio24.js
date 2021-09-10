fecfce565f1ba9e906ef35476c56d45450b97049
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-09-10 14:55:52 -0300

    Desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,15 @@
+db.voos.findOne(
+  { $and: 
+    [
+      { litrosCombustivel: { $not: { $gt: 600 } } }, 
+      { $or: 
+        [
+          { "empresa.nome": { $not: { $eq: "GOL" } } }, 
+          { "empresa.nome": { $not: { $eq: "AZUL" } } },
+        ], 
+      }, 
+    { litrosCombustivel: { $exists: true } },
+    ], 
+  }, 
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
+);
