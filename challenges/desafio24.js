088b41ab1cd83a53ad51c8bed715ae64496ac011
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-10 17:36:26 -0300

    add: req 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,11 @@
+db.voos.findOne(
+  {
+    $and:
+      [
+        { litrosCombustivel: { $not: { $gt: 600 } } },
+        { "empresa.nome": { $ne: { $in: ["GOL", "AZUL"] } } },
+        { litrosCombustivel: { $exists: true } },
+      ],
+  },
+  { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
+);
