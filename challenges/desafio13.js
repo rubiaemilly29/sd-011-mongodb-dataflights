3d9a64f92a113bcb05ea13bad2b577855b42740d
Ghislaine <glenox79@gmail.com>
2021-09-10 18:03:27 -0300

    Desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,4 @@
+db.voos.count({ $and: [
+  { "aeroportoDestino.continente": { $ne: "EUROPA" } },
+  { "aeroportoDestino.continente": { $ne: "ÁSIA" } },
+  { "aeroportoDestino.continente": { $ne: "OCEANIA" } }] }); 
