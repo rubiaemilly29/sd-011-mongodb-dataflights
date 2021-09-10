cf57b8703e246fdb35ca5ad2236a7cbed7e221ee
Hamander <1hamander@gmail.com>
2021-09-10 18:41:26 -0300

    desafio 28 fix part 3

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,13 +1,14 @@
+const NOME_DA_EMPRESA = "LATAM AIRLINES BRASIL";
 db.resumoVoos.insertMany([
   {  
-   empresa: "LATAM AIRLINES BRASIL",
+   empresa: NOME_DA_EMPRESA,
    totalVoosDomesticos: db.voos.find({ $and: [
-    { "empresa.nome": { $eq: "LATAM AIRLINES BRASIL" } }, 
+    { "empresa.nome": { $eq:NOME_DA_EMPRESA } }, 
     { natureza: { $eq: "Doméstica" } }] }).count(),
    },
  ]);
 
-db.resumoVoos.find({ empresa: { $eq: "LATAM AIRLINES BRASIL" } },
+db.resumoVoos.find({ empresa: { $eq: NOME_DA_EMPRESA } },
 { 
 empresa: 1,
 totalVoosDomesticos: 1,
