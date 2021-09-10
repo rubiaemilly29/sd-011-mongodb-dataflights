e9bf7c2ecc76935f9c260af7f1a3bc2844a68b19
marcoswarmling <marcoswarmling15@gmail.com>
2021-09-10 17:15:15 -0300

    desafio 22 e 23 com lint

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,11 @@
-db.voos.find({ litrosCombustivel: { $lte: 1000 , $exists: true } }, { vooId: 1, litrosCombustivel: 1, _id: 0 }).limit(1);
+db.voos.find({
+    litrosCombustivel: {
+        $lte: 1000,
+        $exists: true,
+    },
+},
+    {
+        vooId: 1,
+        litrosCombustivel: 1,
+        _id: 0,
+    }).limit(1);
