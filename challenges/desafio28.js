30b81c19eb55174ee5aeed6f92285defdec1cb93
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-09-10 19:12:04 -0300

    Add Desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,3 @@
+db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: 20026 });
+db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" }, 
+{ empresa: 1, _id: 0, totalVoosDomesticos: 1 });
