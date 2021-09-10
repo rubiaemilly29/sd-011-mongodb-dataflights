1e7956c88185ba947031f5ebe531b5dadcdd5269
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-09-10 19:04:05 -0300

    Add Desafio 26

---
+++ challenges/desafio26.js
@@ -0,0 +1,9 @@
+db.voos.deleteMany(
+    {
+      $and: [
+        { "passageiros.pagos": { $lte: 10 } },
+        { "passageiros.pagos": { $gte: 5 } },
+        { "empresa.nome": "GOL" },
+      ],
+    },
+);
