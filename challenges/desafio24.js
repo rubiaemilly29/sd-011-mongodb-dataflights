5fd22fcf4a51b5403f396bdec621875df06b4eea
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-09-10 17:37:06 -0300

    req24 ok

---
+++ challenges/desafio24.js
@@ -0,0 +1,20 @@
+db.voos.findOne(
+  {
+    $and: 
+      [
+        { litrosCombustivel: { $not: { $gt: 600 } } },
+        { $nor: 
+          [
+            { "empresa.nome": { $in: ["GOL", "AZUL"] } },
+          ],
+        },
+        { litrosCombustivel: { $exists: true } },
+      ],
+  },
+  {
+    _id: 0,
+    vooId: 1,
+    "empresa.nome": 1,
+    litrosCombustivel: 1,
+  },
+);
