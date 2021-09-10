dd91a210721794b8698ccb0143de29cfd355f1ba
Hugosomers <hugosomersla@gmail.com>
2021-09-10 17:50:26 -0300

    Fix LINT

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,5 +1,5 @@
 db.voos.findOne({ $and: 
   [
   { litrosCombustivel: { $lte: 1000 } },
-  { litrosCombustivel : { $exists: true } },
+  { litrosCombustivel: { $exists: true } },
   ] }, { vooId: 1, _id: 0, litrosCombustivel: 1 });
