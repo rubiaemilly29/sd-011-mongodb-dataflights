e82df61a23af526a70998f23aa23de99c16b4450
PH Assis <pieit@live.com>
2021-09-10 15:19:46 -0300

    Challenge 12 ready

---
+++ challenges/desafio11.js
@@ -0,0 +1,4 @@
+// use("dataFlights");
+db.voos
+  .find({ "aeroportoDestino.pais": { $not: { $eq: "ESTADOS UNIDOS" } } })
+  .count();
---
+++ challenges/desafio12.js
@@ -0,0 +1,6 @@
+// use("dataFlights");
+db.voos
+  .find({
+    "aeroportoDestino.pais": { $in: ["BRASIL", "ARGENTINA", "CHILE"] }
+  })
+  .count();
