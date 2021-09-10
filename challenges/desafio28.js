648f837a26f5e477d592f0551b4cec2f9af1b00d
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-09-10 18:35:37 -0300

    Corrige erros lint

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,6 +1,7 @@
-db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL",
-  totalVoosDomesticos: db.voos.find({ "empresa.nome": "LATAM AIRLINES BRASIL",
+const company = "LATAM AIRLINES BRASIL";
+db.resumoVoos.insertOne({ empresa: company,
+  totalVoosDomesticos: db.voos.find({ "empresa.nome": company,
   natureza: "Doméstica" })
  .count() });
-db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" },
+db.resumoVoos.findOne({ empresa: company },
  { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
