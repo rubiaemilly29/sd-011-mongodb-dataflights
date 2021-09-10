cf3be06c2f8787de7e058afcc48c87abe6e8504c
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-09-10 16:55:28 -0300

    Corrigindo erro 18, 19, 20.

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,3 +1,4 @@
 db.voos.find(
   { "passageiros.pagos": { $gt: 7000 } },
+  { _id: 0 },
 ).limit(1);
--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,3 +1,4 @@
 db.voos.find(
   { rtk: { $not: { $exists: true } } },
+  { vooId: 1, _id: 0 },
 ).limit(1);
