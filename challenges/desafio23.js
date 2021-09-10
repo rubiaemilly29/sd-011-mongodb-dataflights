cabc54ac2d5142f29a7a029b9244f05a9164a229
menezesrenan7 <renan_menezes07@hotmail.com>
2021-09-10 17:21:45 -0300

    Ajustes ESLint.

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,5 +1,4 @@
 db.voos.find({
   $and: [
     { litrosCombustivel: { $lte: 1000 } }, 
-    { litrosCombustivel: { $exists: true } }
-  ] }, { _id: 0, litrosCombustivel: 1 }).limit(1);
+    { litrosCombustivel: { $exists: true } }] }, { _id: 0, litrosCombustivel: 1 }).limit(1);
--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,4 +1,5 @@
 db.voos.find(
   { $and: [{ litrosCombustivel: { $lte: 600 } }, 
     { "empresa.name": { $nin: ["GOL", "AZUL"] } }, 
-    { litrosCombustivel: { $exists: true } }] }, { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }).limit(1);
+    { litrosCombustivel: { $exists: true } }] },
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }).limit(1);
