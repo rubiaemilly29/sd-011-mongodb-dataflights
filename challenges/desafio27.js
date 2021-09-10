56f5cae989780ec904f2cba36058e5d8c136666a
menezesrenan7 <renan_menezes07@hotmail.com>
2021-09-10 18:16:15 -0300

    Ajuste ESLint req. 27.

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -2,4 +2,5 @@ db.resumoVoos.insertMany([{
   empresa: "PASSAREDO",
   totalVoosDomesticos: 4187,
 }]);
-db.resumoVoos.find({ empresa: "PASSAREDO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 }).limit(1);
+db.resumoVoos.find({ empresa: "PASSAREDO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 })
+.limit(1);
