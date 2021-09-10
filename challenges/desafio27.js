bef8885d6520a56452d1aa87c6a98ac165f76311
Josan Johnata <josanjohnata@gmail.com>
2021-09-10 19:38:22 -0300

    Desafio 28

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,5 +1,6 @@
 // 27 - Retorne a quantidade total de voos de natureza Doméstica que a empresa PASSAREDO possui, via uso de uma nova coleção chamada resumoVoos.
 
+// 1 QUERY: Conte quantos voos da empresa PASSAREDO cujo campo natureza possua valor igual a Doméstica e crie uma query que insira na coleção resumoVoos um documento com os campos: empresa (nome da empresa) e totalVoosDomesticos (o total retornado anteriormente).
 db.resumoVoos.insertOne(
   {
       empresa: "PASSAREDO",
@@ -11,5 +12,7 @@ db.resumoVoos.insertOne(
       ).count(),
   },
 );
+
+// 1 QUERY: Em uma segunda query, retorne a empresa e o totalVoosDomesticos do primeiro documento presente na coleção resumoVoos em que a empresa seja PASSAREDO.
 
 db.resumoVoos.find({ empresa: "PASSAREDO" }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
---
+++ challenges/desafio28.js
@@ -0,0 +1,30 @@
+// 28 - Retorne a quantidade total de voos de natureza Doméstica que a empresa LATAM AIRLINES BRASIL possui, via uso de uma nova coleção chamada resumoVoos.
+
+// 1 QUERY: Conte quantos voos da empresa LATAM AIRLINES BRASIL cujo campo natureza possua valor igual a Doméstica e crie uma query que insira na coleção resumoVoos um documento com os campos: empresa (nome da empresa) e totalVoosDomesticos (o total retornado anteriormente).
+
+const LATAM = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne(
+    {
+        empresa: LATAM,
+        totalVoosDomesticos: db.voos.find(
+            {
+                "empresa.nome": LATAM,
+                natureza: "Doméstica",
+            },
+        ).count(),
+    },
+);
+
+// 2 QUERY: Em uma segunda query, retorne a empresa e o totalVoosDomesticos do primeiro documento presente na coleção resumoVoos em que a empresa seja LATAM AIRLINES BRASIL.
+
+db.resumoVoos.find(
+  {
+      empresa: LATAM,
+  },
+  {
+      empresa: 1,
+      totalVoosDomesticos: 1,
+      _id: 0,
+  },
+).limit(1);
