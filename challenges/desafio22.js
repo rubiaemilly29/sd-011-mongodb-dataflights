daa4c575f1a7a6f5ee340070ab7ee50c86738c65
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-10 20:53:17 -0300

    desafio 23

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,5 +1,5 @@
-db.voos.findOne({ $and: [{ "aeroportoOrigem.sigla": { $eq: "SBGR"} }, 
-{ "aeroportoDestino.sigla": { $eq: "KJFK"} }, 
+db.voos.findOne({ $and: [{ "aeroportoOrigem.sigla": { $eq: "SBGR" } }, 
+{ "aeroportoDestino.sigla": { $eq: "KJFK" } }, 
 { $or: [{ "empresa.nome": { $eq: "DELTA AIRLINES" } }, 
-{ "empresa.nome": { $eq: "AMERICAN AIRLINES"} }] }] }, 
-{ _id: 0, vooId: 1});
+{ "empresa.nome": { $eq: "AMERICAN AIRLINES" } }] }] }, 
+{ _id: 0, vooId: 1 });
