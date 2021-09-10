26b01288b62f6de45d9e4a2e8c120821a9b53b7e
menezesrenan7 <renan_menezes07@hotmail.com>
2021-09-10 17:24:36 -0300

    Ajustes ESLint.

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -4,7 +4,8 @@ db.voos.find({
   { ano: 2017 }] }, 
 { 
   _id: 0, 
-  vooId: 1, "empresa.nome": 1,
+  vooId: 1,
+  "empresa.nome": 1,
   "aeroportoOrigem.nome": 1,
   "aeroportoDestino.nome": 1,
   mes: 1,
--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -2,4 +2,5 @@ db.voos.find(
   { $and: [{ litrosCombustivel: { $lte: 600 } }, 
     { "empresa.name": { $nin: ["GOL", "AZUL"] } }, 
     { litrosCombustivel: { $exists: true } }] },
-    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }).limit(1);
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }
+    ).limit(1);
