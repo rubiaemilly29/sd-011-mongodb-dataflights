52dc8ca901022c0e94d60180f5b7edc125db084c
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-09-11 12:46:56 -0300

    adiciona desafios 21-25

---
+++ challenges/desafio23.js
@@ -0,0 +1,3 @@
+// 23 - Retorne o vooId e litrosCombustivel do primeiro voo em que o campo litrosCombustivel não seja maior do que 1000 e o campo litrosCombustivel exista.
+db.voos.findOne({ litrosCombustivel: { $not: { $gt: 1000 }, $exists: true } },
+  { vooId: 1, litrosCombustivel: 1, _id: 0 });
---
+++ challenges/desafio24.js
@@ -0,0 +1,4 @@
+// 24 - Retorne o vooId, empresa.nome e litrosCombustivel do primeiro voo em que litrosCombustivel não seja maior do que 600 e a empresa não seja GOL ou AZUL, e o campo litrosCombustivel exista.
+db.voos.findOne({ $and: [{ litrosCombustivel: { $not: { $gt: 600 }, $exists: true } },
+  { "empresa.nome": { $nin: ["GOL", "AZUL"] } }] },
+  { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
