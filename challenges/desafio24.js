5b2c2651ca08a7ebdac1c19ab724c22c9511c06a
Daniel Leon <daniel.radialista@gmail.com>
2021-09-10 21:06:50 -0300

    desafio 24 pronto e rodando nos testes locais

---
+++ challenges/desafio24.js
@@ -0,0 +1,4 @@
+db.voos.findOne({ 
+  $and: [{ litrosCombustivel: { $lt: 600 } },
+  { empresa: { $nin: ["AZUL", "GOL"] } }, { litrosCombustivel: { $exists: true } }] },
+  { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
