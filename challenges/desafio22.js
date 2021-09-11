e2b5e5686b2d4d94d3f0e2bc7a3f62f12654eb5a
IsaacMagno <isaacmagno98@gmail.com>
2021-09-11 11:49:02 -0300

    desafio22 cor

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,4 +1,4 @@
 db.voos.find({ $and:
   [{ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
   { "aeroportoOrigem.sigla": { $eq: "SBGR" } },
-  { "aeroportoDestino.sigla": { $eq: "KJFK" }}] }, { vooId: 1, _id: 0 }).limit(1);
+  { "aeroportoDestino.sigla": { $eq: "KJFK" } }] }, { vooId: 1, _id: 0 }).limit(1);
