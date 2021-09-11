388d9055629217e463a460fc337301761d35a121
gabiazevedo <gabiazevedoms@gmail.com>
2021-09-11 16:18:38 -0300

    Finaliza Desafio 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -2,4 +2,4 @@ db.voos.findOne({ $and:
   [{ $nor: [{ "empresa.nome": { $eq: "GOL" } }, { "empresa.nome": { $eq: "AZUL" } }] },
   { litrosCombustivel: { $exists: true } },
   { litrosCombustivel: { $not: { $gt: 600 } } }] },
-  { _id: 0, vooId: 1, litrosCombustivel: 1 });
+  { _id: 0, vooId: 1, litrosCombustivel: 1, "empresa.nome": 1 });
