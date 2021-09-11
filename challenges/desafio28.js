3b0201df8f5318fdfc6f85b2d643121ce3f70877
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-11 19:17:33 -0300

    desafio 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,6 @@
+const empresaLatam = "LATAM AIRLINES BRASIL";
+db.resumoVoos.insert({ empresa: empresaLatam },
+{ totalVoosDomesticos: db.voos.count({ "empresa.nome": empresaLatam, 
+natureza: "Doméstica" }) });
+db.resumoVoos.findOne({ 
+empresa: empresaLatam }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 }); 
