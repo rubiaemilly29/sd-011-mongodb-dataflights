df79dd088a6029fbce96ab9d08c5c5a408140b20
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-11 19:04:26 -0300

    desafio 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,3 +1,3 @@
 db.resumoVoos.insert({ empresa: "PASSAREDO" },
 { totalVoosDomesticos: db.voos.count({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" }) });
-db.resumoVoos.find({ empresa: "PASSAREDO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 }); 
+db.resumoVoos.findOne({ empresa: "PASSAREDO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 }); 
