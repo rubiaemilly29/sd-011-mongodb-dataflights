ed9873bb5cb3710b6e0ddc778235aff4179ba5b3
gab-boff <gmeninboff@gmail.com>
2021-09-11 19:17:09 -0300

    Desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,9 @@
+db.voos.find(
+    { $and: [
+        { litrosCombustivel: { $lte: 600 } },
+        { litrosCombustivel: { $exists: true } },
+        { $or: [{ "empresa.nome": { $not: { $eq: "GOL" } } },
+        { "empresa.nome": { $not: { $eq: "AZUL" } } }] },
+    ] },
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
+).limit(1);
