b29e19b72f7bbe48ff066befa6fe83f4199b0b04
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-11 10:23:11 -0300

    desafio 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,6 +1,5 @@
-db.voos.findOne({ $and: [
-{ litrosCombustivel: { $not: { $gt: 600 } } },
-{ "empresa.nome": { $in: ["GOL", "AZUL"] } },
-{ litrosCombustivel: { $exists: true } },
-] },
-{ _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });
+db.voos.findOne({
+    litrosCombustivel: { $lte: 600 },
+    "empresa.nome": { $nin: ["GOL", "AZUL"] },
+  },
+  { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });
