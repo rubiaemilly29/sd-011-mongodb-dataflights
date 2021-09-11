b9be87317c5f42b2f9c2bdf1dbd035dfb0e4814e
jhenriquemargrafm <jh.margraf@gmail.com>
2021-09-11 11:46:15 -0300

    Requisitos 21 a 26

---
+++ challenges/desafio22.js
@@ -0,0 +1,5 @@
+db.voos.findOne({ "empresa.nome": { $in: ["AMERICAN AIRLINES", "DELTA AIRLINES"] }, 
+  "aeroportoOrigem.sigla": "SBGR",
+  "aeroportoDestino.sigla": "KJFK",
+},
+  { _id: 0, vooId: 1 });
---
+++ challenges/desafio24.js
@@ -0,0 +1,3 @@
+db.voos.findOne({ $nor: [{ "empresa.nome": ["GOL", "AZUL"] }],
+  litrosCombustivel: { $exists: 1, $lt: 600 } },
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });
