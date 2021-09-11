162006da6218066da3867a5f938341381bbc457c
Daniel Leon <daniel.radialista@gmail.com>
2021-09-10 23:06:32 -0300

    trocado o nome da constante do desafio 28, projeto ja concluido com sucesso

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,8 +1,8 @@
-const companhiaAerea = "LATAM AIRLINES BRASIL";
+const aerea = "LATAM AIRLINES BRASIL";
 
 db.resumoVoos.insertOne({
-  empresa: companhiaAerea,
-  totalVoosDomesticos: db.voos.count({ "empresa.nome": companhiaAerea, natureza: "Doméstica" }),
+  empresa: aerea,
+  totalVoosDomesticos: db.voos.count({ "empresa.nome": aerea, natureza: "Doméstica" }),
 });
 
-db.resumoVoos.find({ empresa: companhiaAerea }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
+db.resumoVoos.find({ empresa: aerea }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
