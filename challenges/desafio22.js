f569d8ea2b64bbb8142a1e8f6a2dddc65a0d9af2
AlexSmith7192 <alexsmith7192@gmail.com>
2021-09-10 23:32:51 -0300

    22 - resolves challenge twenty two

---
+++ challenges/desafio22.js
@@ -0,0 +1,16 @@
+db.voos.find(
+  {
+    $or: [
+      { "empresa.nome": { $eq: "DELTA AIRLINES" } },
+      { "empresa.nome": { $eq: "AMERICAN AIRLINES" } },
+    ],
+    $and: [
+      { "aeroportoOrigem.sigla": { $eq: "SBGR" } },
+      { "aeroportoDestino.sigla": { $eq: "KJFK" } },
+    ],
+  },
+  {
+    _id: 0,
+    vooId: 1,
+  },
+).limit(1);
