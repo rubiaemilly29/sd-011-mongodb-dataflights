250994ec455f52fc63f19fc67b7eaa47458ca91f
Marconi <marconieng@gmail.com>
2021-09-11 23:04:22 -0300

    Corrigindo condicao

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -2,7 +2,7 @@ db.voos.find(
   { $and:
     [
       { litrosCombustivel: { $exists: true } },
-      { litrosCombustivel: { $lt: 1000 } },
+      { litrosCombustivel: { $lte: 1000 } },
     ] },
       { _id: 0, vooId: 1, litrosCombustivel: 1 },
 ).limit(1);
