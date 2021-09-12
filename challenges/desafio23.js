d2ae5b59ef446f6633049e655965be42159f33a4
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-09-11 23:13:00 -0300

    Corrigindo desafio 23!

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,5 +1,5 @@
 db.voos.find({
-  $and: [{ litrosCombustivel: { $lt: 1000 } },
+  $and: [{ litrosCombustivel: { $lte: 1000 } },
     { litrosCombustivel: { $exists: true } },
   ],
 }, { _id: 0, vooId: 1, litrosCombustivel: 1 }).limit(1);
