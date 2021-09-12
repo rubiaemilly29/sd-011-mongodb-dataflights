7017f42c3ebd9159ba6400f6e6990e8fe2d6e148
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-09-12 18:09:32 -0300

    Added req 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,7 @@
+db.req28.insertOne({
+  empresa: "LATAM AIRLINES BRASIL", 
+  totalVoosDomesticos: db.voos.find({ "empresa.nome": "LATAM AIRLINES BRASIL",
+  natureza: "Doméstica" }).count(),
+});
+
+db.req28.find({}, { _id: 0 });
