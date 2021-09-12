18d017c7c206e144220cb9d924f32aa3cd9a6dbc
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-12 12:39:02 -0300

    Requisito 27 feito

---
+++ challenges/desafio27.js
@@ -0,0 +1,8 @@
+db.voos.count({ $and: [{ "empresa.nome": "PASSAREDO" }, { natureza: "Doméstica" }] });
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.count(
+    { $and: [{ "empresa.nome": "PASSAREDO" }, { natureza: "Doméstica" }] },
+  ),
+});
+db.resumoVoos.findOne({ empresa: "PASSAREDO" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
