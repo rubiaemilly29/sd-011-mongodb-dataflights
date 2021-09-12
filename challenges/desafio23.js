2c37e560f2b58765837ddb19c6454a929c6e382a
Marconi <marconieng@gmail.com>
2021-09-11 22:08:25 -0300

    Alterando para menor que

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -2,7 +2,7 @@ db.voos.find(
   { $and:
     [
       { litrosCombustivel: { $exists: true } },
-      { litrosCombustivel: { $lte: 1000 } },
+      { litrosCombustivel: { $lt: 1000 } },
     ] },
       { _id: 0, vooId: 1 },
 ).limit(1);
