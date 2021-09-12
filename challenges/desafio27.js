7ff78066f22cbe7e2f3b7d591e6f94ed6b88a123
jhenriquemargrafm <jh.margraf@gmail.com>
2021-09-11 22:04:44 -0300

    Ajuste nome de variaveis

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,6 +1,7 @@
-const voosPASSAREDODom = db.voos
-  .count({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" });
+const totalVoosDomesticos = db.voos.count(
+  { "empresa.nome": "PASSAREDO", natureza: "Doméstica" },
+);
 
-db.resumoVoos.insertOne({ empresa: "PASSAREDO", voosPASSAREDODom });
+db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomesticos });
 
-db.resumoVoos.find({}, { _id: 0, empresa: 1, voosPASSAREDODom: 1 });
+db.resumoVoos.find({}, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,6 +1,7 @@
-const voosLATAMDom = db.voos
-  .count({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" });
+const totalVoosDomesticos = db.voos.count(
+  { "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" },
+);
 
-db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", voosLATAMDom });
+db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos });
 
-db.resumoVoos.find({}, { _id: 0, empresa: 1, voosLATAMDom: 1 });
+db.resumoVoos.find({}, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
