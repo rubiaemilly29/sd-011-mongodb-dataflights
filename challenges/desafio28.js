cef3a9f7f6e7e181a8c50ac18ae228d5ac2e529e
jorgemeyrelles <jotaengpuc@gmail.com>
2021-09-12 16:44:16 -0300

    feat: desafios 27 e 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,13 @@
+const latam = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne({
+  empresa: latam,
+  totalVoosDomesticos: db.voos
+    .find({ "empresa.nome": latam, natureza: "Doméstica" })
+    .count(),
+});
+
+db.resumoVoos.find(
+  { empresa: latam },
+  { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+);
