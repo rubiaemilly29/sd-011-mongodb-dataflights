0c65aaa61c5ea50a492642b46c4892c50198a0c9
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-09-11 23:17:59 -0300

    Desafio 24 finalizado e corrigindo edentação do 23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,5 +1,6 @@
 db.voos.find({
-  $and: [{ litrosCombustivel: { $lte: 1000 } },
+  $and: [
+    { litrosCombustivel: { $lte: 1000 } },
     { litrosCombustivel: { $exists: true } },
   ],
 }, { _id: 0, vooId: 1, litrosCombustivel: 1 }).limit(1);
