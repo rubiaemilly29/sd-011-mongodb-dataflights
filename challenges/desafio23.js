bfd6e2d48f5ff11aef509740d8f2053b2cb6a978
Marconi <marconieng@gmail.com>
2021-09-11 22:33:05 -0300

    Retornando a chave litrosCombustivel

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -4,5 +4,5 @@ db.voos.find(
       { litrosCombustivel: { $exists: true } },
       { litrosCombustivel: { $lt: 1000 } },
     ] },
-      { _id: 0, vooId: 1 },
+      { _id: 0, vooId: 1, litrosCombustivel: 1 },
 ).limit(1);
