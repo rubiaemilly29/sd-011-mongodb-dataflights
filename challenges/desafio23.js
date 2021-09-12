ffc0646ceea0763e0d17214c6f27fd198b57cd98
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-12 19:22:30 -0300

    corrigindo 23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,1 +1,4 @@
-db.voos.findOne({ litrosCombustivel: { $exists: { $lt: 1000 } } }, { vooId: 1, _id: 0, litrosCombustivel: 1 });
+db.voos.findOne({ 
+  litrosCombustivel:
+  { $exists: { $lt: 1000 } } }, 
+  { vooId: 1, _id: 0, litrosCombustivel: 1 });
