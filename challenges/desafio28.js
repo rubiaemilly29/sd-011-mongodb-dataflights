4b213c2827a89554335faf482ef9e42693e12bc4
Daniel <danielfdoficial@gmail.com>
2021-09-12 14:47:05 -0300

    add desafios 27 e 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,15 @@
+const latamBRASIL = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne(
+  {
+    empresa: latamBRASIL,
+    totalVoosDomesticos: db.voos.count(
+      {
+        "empresa.nome": "LATAM AIRLINES BRASIL",
+        natureza: "Doméstica",
+      },
+      ),
+  },
+  );
+
+db.resumoVoos.findOne({ empresa: latamBRASIL }, { empresa: 1, totalVoosDomesticos: 1, _id: 0 });
