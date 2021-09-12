0bb2cd2139f7ac896825b962edd6b57a59740a1b
Marcelo <marsleite@gmail.com>
2021-09-12 18:23:36 -0300

    desafio 27 feito

---
+++ challenges/desafio27.js
@@ -0,0 +1,6 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.count({ "empresa.nome": "PASSAREDO", natureza: "Doméstica" }),
+});
+
+db.resumoVoos.find({}, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
