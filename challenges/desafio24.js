1526db8b3a50bbec4d3e6fb53e8b60a3c1911a59
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-13 12:07:09 -0300

    Desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,3 @@
+db.voos.find({ "empresa.nome": { $nin: ["GOL", "AZUL"] },
+litrosCombustivel: { $exists: true, $lt: 600 } },
+{ vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 }).limit(1);
