dfd6b71b16529952edf907869bfe7658032bb4ce
Marconi <marconieng@gmail.com>
2021-09-12 21:30:24 -0300

    Corrigindo condicao

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,1 +1,8 @@
-db.voos.deleteMany({ "empresa.nome": "GOL", "passageiros.pagos": { $in: [5, 10] } });
+db.voos.deleteMany(
+  { $and:
+    [
+      { "empresa.nome": "GOL" },
+      { "passageiros.pagos": { $gte: 5 } },
+      { "passageiros.pagos": { $lte: 10 } },
+    ] },
+);
