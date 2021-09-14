70b2ee55470de08f20147078db15c707ae6e92b2
Vinicius <viniciusfod@gmail.com>
2021-09-13 21:24:38 -0300

    req22-fix

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -2,7 +2,7 @@ db.voos.find({
     $and: 
     [{ $or:
          [{ "empesa.nome": "DELTA AIRLINES" },
-          { "empesa.nome": "AMERICAN AIRLINES" },
+          { "empesa.nome": "AMERICAN AIRLINES" }] },
           { "aeroportoOrigem.sigla": "SBGR" },
-          { "aeroportoDestino.sigla": "KJFK" }] }] },
+          { "aeroportoDestino.sigla": "KJFK" }] },
            { _id: 0, vooId: 1 }).limit(1);
