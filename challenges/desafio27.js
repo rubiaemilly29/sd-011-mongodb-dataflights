a08f945ea2abdf8dec71e7d27916c027ce6c70f5
Gustavo Lemes <gustavolemes997@gmail.com>
2021-09-14 16:50:36 -0300

    Requisito 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,15 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.count({ natureza: "Doméstica", "empresa.nome": "PASSAREDO" }),
+});
+
+db.resumoVoos.find(
+  {
+    empresa: "PASSAREDO"
+  },
+  {
+    empresa: 1,
+    totalVoosDomesticos: 1,
+    _id: 0
+  }
+);
