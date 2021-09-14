1289c60a2a6204d6781d32faa8702f28295e3f54
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-09-14 19:17:05 -0300

    desafio28

---
+++ challenges/desafio28.js
@@ -0,0 +1,16 @@
+const EMPRESA = "LATAM AIRLINES BRASIL";
+db.resumoVoos.insertOne({
+  empresa: EMPRESA,
+  totalVoosDomesticos: db.voos.count({ natureza: "Doméstica", "empresa.nome": EMPRESA }),
+});
+
+db.resumoVoos.find(
+  {
+    empresa: EMPRESA,
+  },
+  {
+    empresa: 1,
+    totalVoosDomesticos: 1,
+    _id: 0,
+  },
+);
