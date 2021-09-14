b9900839dfbf0622278b6932bcf17da4ad98de5f
Rafael Schmitz <schimitaum@gmail.com>
2021-09-13 22:11:47 -0300

    6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,3 +1,1 @@
-// Retorne apenas os campos empresa.sigla, empresa.nome e passageiros do voo com o campo vooId igual a 756807
-
-db.voos.find({}, { vooId: 1, _id: 0 }).limit(3).skip(9);
+db.voos.find({ vooId: 756807 }, { "empresa.sigla": 1, "empresa.nome": 1, passageiros: 1, _id: 0 });
