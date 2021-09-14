c362bc9043fb2f7428332a11fe016fd2da12cdf6
Rafael Schmitz <schimitaum@gmail.com>
2021-09-13 23:47:17 -0300

    18, 21, 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,9 @@
+db.voos.findOne({ $and: [ 
+  { $or: [{ empresa: "DELTA AIRLINES" }, { empresa: "AMERICAN AIRLINES" }] },
+  { "aeroportoOrigem.sigla": SBGR },
+  { "aeroportoDestino.sigla": KJFK },
+] },  
+{ 
+  vooId: 1,
+  _id: 0,
+});
