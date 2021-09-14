e49364537baa95e581682a9fc70638a35632b20c
LeoLacana <wilson17414@gmail.com>
2021-09-14 17:46:12 -0300

    requisito_24_feito

---
+++ challenges/desafio24.js
@@ -0,0 +1,5 @@
+db.voos.find({ $and: [
+    { litrosCombustivel: { $not: { $gt: 600 } } },
+    { litrosCombustivel: { $exists: true } },
+    { "empresa.nome": { $nin: ["GOL", "AZUL"] } }],
+}, { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }).limit(1);
