a503a58315e3b1e91abea14f40e258d0334b2cae
Vinicius <viniciusfod@gmail.com>
2021-09-13 21:45:07 -0300

    req28

---
+++ challenges/desafio28.js
@@ -0,0 +1,13 @@
+const companyName = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne(
+  {
+    empresa: companyName,
+    totalVoosDomesticos: db.voos.find({ 
+      $and: [{ "empresa.nome": companyName }, { natureza: "Doméstica" }] }).count(),
+  },
+);
+
+db.resumoVoos.find(
+  { empresa: companyName }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+);
